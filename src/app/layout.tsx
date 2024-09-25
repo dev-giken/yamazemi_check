cbd54dda61fa510f4bf278792478401cb3e2e14b
koi-ama-m3p <koiama@amakasukiichirounoMacBook-Pro.local>
2024-09-26 01:03:39 +0900

    new development branch--- fixed for mobile screen 1.2.1  deleted unused viewport-fit=cover

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className={inter.className}>
         <Header />
