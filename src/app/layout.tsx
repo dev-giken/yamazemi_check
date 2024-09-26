c696cb4259088f740756c9702b3ae18b5ccafcc0
koi-ama-m3p <koichiro920@gmail.com>
2024-09-27 05:56:40 +0900

    fixed meta info 2.0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,51 @@
 import { Inter } from "next/font/google";
 import Header from "@/components/ui/header";
 import Footer from "@/components/ui/footer";
-import { SeoHead } from "@/components/seoHead";  // SeoHeadのインポート
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "慶應義塾大学商学部 山本勲研究会",
+  description: "山ゼミ | 計量経済学をツールに総合力を身につける",
+  metadataBase: new URL("https://www.yamazemi.info"),
+  openGraph: {
+    title: "慶應義塾大学商学部 山本勲研究会",
+    description: "計量経済学をツールに総合力を身につける",
+    url: "/",
+    images: [
+      {
+        url: "/images/seminar_square_logo.png",
+        width: 500,
+        height: 500,
+        alt: "山本勲研究会",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@yamazemi2024",
+    title: "慶應義塾大学商学部 山本勲研究会",
+    description: "計量経済学をツールに総合力を身につける",
+    images: [
+      {
+        url: "/images/seminar_square_logo.png",
+        alt: "山本勲研究会",
+      },
+    ],
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // 環境変数からpageOgImgを設定
-  const pageOgImg: string = `${process.env.NEXT_PUBLIC_DEFAULT_SITE_URL}`;
-
   return (
     <html lang="ja">
       <head>
-        {/* SeoHeadコンポーネントを使用 */}
-        <SeoHead
-          title="慶應義塾大学商学部"
-          titleTemplate="山本勲研究会"
-          description="山ゼミ | 計量経済学をツールに総合力を身につける"
-          ogType="website"
-          // pageOgImg変数を使用して画像URLを設定
-          imgUrl={`${pageOgImg}/images/seminar_square_logo.png`}
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className={inter.className}>
         <Header />
--- src/types/metatypes.ts
+++
@@ -1,7 +0,0 @@
-export type MetaTypes = {
-    title?: string;
-    titleTemplate?: string;
-    description?: string;
-    ogType?: string;
-    imgUrl?: string;
-  };
