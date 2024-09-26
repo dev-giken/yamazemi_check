e728f97d2218ff6e1671f5f620ac6898bf43ae5b
koi-ama-m3p <koichiro920@gmail.com>
2024-09-27 05:37:47 +0900

    fixed meta info 1.2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import Header from "@/components/ui/header";
 import Footer from "@/components/ui/footer";
-import { SeoHead } from "@/components/seoHead";  // Import SeoHead component
+import { SeoHead } from "@/components/seoHead";  // SeoHeadのインポート
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,16 +11,20 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // 環境変数からpageOgImgを設定
+  const pageOgImg: string = `${process.env.NEXT_PUBLIC_DEFAULT_SITE_URL}`;
+
   return (
     <html lang="ja">
       <head>
-        {/* Use SeoHead component instead of Metadata */}
+        {/* SeoHeadコンポーネントを使用 */}
         <SeoHead
-          title="慶應義塾大学商学部 山本勲研究会"
-          titleTemplate="慶應義塾大学商学部 山本勲研究会"
+          title="慶應義塾大学商学部"
+          titleTemplate="山本勲研究会"
           description="山ゼミ | 計量経済学をツールに総合力を身につける"
           ogType="website"
-          imgUrl="/images/seminar_square_logo.png"
+          // pageOgImg変数を使用して画像URLを設定
+          imgUrl={`${pageOgImg}/images/seminar_square_logo.png`}
         />
       </head>
       <body className={inter.className}>
