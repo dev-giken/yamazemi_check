5cfa763f70fa6682b3dcb4e718d56db79e836d36
koi-ama-m3p <koichiro920@gmail.com>
2024-09-29 11:40:35 +0900

    added icon.svg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,6 +48,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ja">
       <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body className={inter.className}>
