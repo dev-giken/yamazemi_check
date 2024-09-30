82ead0c5a8b0199123071bac5eeed6fb30311165
koi-ama-m3p <koichiro920@gmail.com>
2024-09-30 17:40:52 +0900

    added top page contents

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,38 @@ import { Button } from "@/components/ui/button";
 import Hero from "@/components/ui/hero";
 import NewsSection from '@/components/ui/NewsSection';
 import { client } from "@/lib/client";
+import styles from '../styles/ArticleContent.module.css';
+
+type MainContent = {
+  id: string;
+  title: string;
+  category_1: string;
+  category_2: string;
+  contents: string;
+};
 
 export default async function Home() {
-  const data = await client.get({ endpoint: 'news' });
+  // `news` エンドポイントからニュースデータを取得
+  const newsData = await client.get({ endpoint: 'news' });
 
-  const newsItems = data.contents.slice(0, 3).map((item: any) => ({
+  // 新しい `main` エンドポイントからトップページ用データを取得
+  const mainData = await client.get({
+    endpoint: 'main',
+    queries:{
+      limit: 20,
+    },
+  });
+
+  // 必要なデータを整理する
+  const mainItems = mainData.contents.map((item: MainContent) => ({
+    id: item.id,
+    title: item.title,
+    category_1: item.category_1,
+    category_2: item.category_2,
+    contents: item.contents,
+  }));
+
+  const newsItems = newsData.contents.slice(0, 3).map((item: any) => ({
     id: item.id,
     title: item.title,
     publishedAt: item.publishedAt,
@@ -32,8 +59,23 @@ export default async function Home() {
         </div>
         <div className="flex flex-col items-center justify-between">
           <NewsSection newsItems={newsItems} />
-          <div className="flex justify-center items-center h-screen p-36">
-            <Button>準備中</Button>
+          <div className="p-10 text-xs">
+            {mainItems
+              .filter((item: MainContent) => {
+                console.log(item);
+                return item.category_1.includes('トップ') && item.category_2.includes('none');
+              })
+              .map((item: MainContent) => {
+                console.log("Mapping item:", item);
+                return (
+                  <div key={item.id}>
+                    {/*<h3>{item.title}</h3>*/}
+                    <div className={styles.articleContent}>
+                      <div dangerouslySetInnerHTML={{ __html: item.contents }} />
+                    </div>
+                  </div>
+                );
+            })}
           </div>
         </div>
       </main>
