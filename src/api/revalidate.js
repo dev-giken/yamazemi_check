edf4d0365430cf0d0e95b53ea4a5e6e82010deae
koi-ama-m3p <koichiro920@gmail.com>
2024-10-18 00:58:38 +0900

    checked blog update ability

--- src/api/revalidate.js
+++ src/api/revalidate.js
@@ -7,6 +7,14 @@ export default async function handler(req, res) {
     try {
       // 指定のページのキャッシュをクリアして再生成
       await res.revalidate('/'); // '/'はトップページ。再生成したいパスを指定
+      await res.revalidate('/news');
+      if (req.query.newsId) {
+        await res.revalidate(`/news/${req.query.newsId}`);
+      }
+      await res.revalidate('/blog');
+      if (req.query.blogId) {
+        await res.revalidate(`/blog/${req.query.blogId}`); // Revalidate specific blog posts if an ID is provided
+      }
       return res.json({ revalidated: true });
     } catch (err) {
       return res.status(500).send('Error revalidating');
