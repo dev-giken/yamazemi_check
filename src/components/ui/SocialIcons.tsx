6a5f72a1dc8e272254ec10a667ea95e2df371a1a
koi-ama <167736150+koi-ama@users.noreply.github.com>
2025-05-29 15:02:23 +0900

    Update SocialIcons.tsx
    年度変わり。各種アカウント名に含まれるyyyyを更新

--- src/components/ui/SocialIcons.tsx
+++ src/components/ui/SocialIcons.tsx
@@ -5,10 +5,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const SocialIcons: FC = () => (
   <div className="flex space-x-4">
-    <a href="https://x.com/yamazemi2024" target="_blank" rel="noopener noreferrer">
+    <a href="https://x.com/yamazemi2025" target="_blank" rel="noopener noreferrer">
       <FontAwesomeIcon icon={faXTwitter} className="w-6 h-6 text-white hover:text-gray-400" />
     </a>
-    <a href="https://www.instagram.com/yamazemi2024/" target="_blank" rel="noopener noreferrer">
+    <a href="https://www.instagram.com/yamazemi2025/" target="_blank" rel="noopener noreferrer">
       <FontAwesomeIcon icon={faInstagram} className="w-6 h-6 text-white hover:text-gray-400" />
     </a>
     <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="hidden"> {/* hiddenに設定 */}
